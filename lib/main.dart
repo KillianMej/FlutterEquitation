56644384d8aeaecfa64fd87003b38248699d79fd
BaptisteCMY <144695314+BaptisteCMY@users.noreply.github.com>
2025-03-05 15:36:25 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,41 @@ Future<void> initDb() async {
   print("📂 La base de données sera créée à : $dbPath");
 
   final database = await openDatabase(
-    dbPath,
+    join(await getDatabasesPath(), 'database.db'),
     onCreate: (db, version) async {
       await db.execute(
-        'CREATE TABLE utilisateur(id INTEGER PRIMARY KEY AUTOINCREMENT, nom VARCHAR(100) NOT NULL, email VARCHAR(255) UNIQUE NOT NULL, mot_de_passe VARCHAR(255) NOT NULL, numero VARCHAR(20), age INT, ffe VARCHAR(255), photo VARCHAR(255), gerant BOOLEAN)',
+        'CREATE TABLE utilisateur(id INTEGER PRIMARY KEY AUTOINCREMENT, nom VARCHAR(100) NOT NULL, email VARCHAR(255) UNIQUE NOT NULL, mot_de_passe VARCHAR(255) NOT NULL, numero VARCHAR(20), age INT, ffe VARCHAR(255),photo VARCHAR(255), gerant BOOLEAN)',
       );
-      // Création des autres tables si nécessaire
       await db.execute(
         'CREATE TABLE cheval(id INTEGER PRIMARY KEY AUTOINCREMENT,photo VARCHAR(255),nom VARCHAR(100) NOT NULL,age INT,robe VARCHAR(50),race VARCHAR(100),sexe VARCHAR(1) NOT NULL,specialite_id INT,utilisateur_id INT)',
       );
-      // Autres créations de tables...
-
+      await db.execute(
+        'CREATE TABLE specialites (id INTEGER PRIMARY KEY AUTOINCREMENT,nom VARCHAR(100) NOT NULL);'
+      );
+      await db.execute(
+        'CREATE TABLE cours (id INTEGER PRIMARY KEY AUTOINCREMENT,terrain VARCHAR(8),date DATETIME NOT NULL,duree INT,specialite_id INT,valide INT)'
+      );
+      await db.execute(
+        'CREATE TABLE concours (id INTEGER PRIMARY KEY AUTOINCREMENT,nom VARCHAR(100) NOT NULL,adresse VARCHAR(255),photo VARCHAR(255),date DATETIME NOT NULL,niveau_id INT)'
+      );
+      await db.execute(
+        'CREATE TABLE niveau (id INTEGER PRIMARY KEY AUTOINCREMENT,nom VARCHAR(100) NOT NULL);'
+      );
+      await db.execute(
+        'CREATE TABLE participant (id INTEGER PRIMARY KEY AUTOINCREMENT,cours_id INT,concours_id INT,soiree_id INT,utilisateur_id INT,commentaire TEXT)'
+      );
+      await db.execute(
+        'CREATE TABLE soiree (id INTEGER PRIMARY KEY AUTOINCREMENT,theme_id INT,photo VARCHAR(255))'
+      );
+      await db.execute(
+        'CREATE TABLE theme (id INTEGER PRIMARY KEY AUTOINCREMENT,nom VARCHAR(100) NOT NULL);'
+      );
+      await db.execute(
+        'CREATE TABLE dp (id INTEGER PRIMARY KEY AUTOINCREMENT,cheval_id INT,utilisateur_id INT);'
+      );
     },
     version: 1,
   );
-
   print("🚀 Base de données prête !");
 }
 
